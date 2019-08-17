9a157b364079130507eeda63a2d2598310263601
Dave C <craftyfella@gmail.com>
2019-08-17 11:43:48 +0100

    Tidyup the unused fields

--- src/scalyrTransport.ts
+++ src/scalyrTransport.ts
@@ -6,9 +6,7 @@ export const delay = (ms: number) => {
   return new Promise( resolve => setTimeout(resolve, ms) );
 }
 export class ScalyrTransport extends Transport {
-  options: ScalyrTransportOptions
   queue: Array<any> = []
-  maxBatchSize: number
   frequencyMs: number
   running: boolean = true
   sendLogs: BatchingScalyrEventsSender
@@ -16,14 +14,11 @@ export class ScalyrTransport extends Transport {
   constructor(options: ScalyrTransportOptions) {
     super()
     this.level = options.level || 'verbose'
-    this.maxBatchSize = options.maxBatchSize || 100
     this.frequencyMs = options.frequencyMs || 5000
-    this.options = options
+    this.sendLogs = createBatchingEventsSender(options)
     if (options.autoStart || true) {
       this.startPolling()
     }
-    this.sendLogs = createBatchingEventsSender(this.options)
-
   }
 
   log(info: any, next: () => void) {
